87282b89a3d1c7c0dcd6f95193327fce761f8671
Mike <mike@koolaborate.com>
2013-11-02 20:33:03 +0800

    Move files around, set up core infrastructure

--- loanwolf/src/com/koolaborate/sys/controller/LoginController.java
+++ loanwolf/src/com/koolaborate/sys/controller/LoginController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
  
-	@RequestMapping({"/","/welcome"})
-	public String printWelcome(ModelMap model) {
+	@RequestMapping({"/login"})
+	public String showLogin(ModelMap model) {
 		System.out.println("inside");
 		model.addAttribute("message", "Spring 3 MVC Hello World");
 		return "login";
- 
+	}
+	
+	@RequestMapping({"/demo"})
+	public String showDemo(ModelMap model) {
+		System.out.println("inside");
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "demo";
 	}
  
 }
